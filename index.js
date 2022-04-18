27ab3899f45e36ea3ade84f8bfb840f936e6443c
Icy-cui <icercyf@gmail.com>
2022-04-18 13:40:58 +0800

    update: write html file and some comments

--- index.js
+++ index.js
@@ -1,63 +1,74 @@
 const fs = require("fs");
-
 const path = require("path");
 
-// fs.readFile(__dirname + "/2.txt", "utf8", function (err, data) {
-//   if (err) {
-//     return console.error(err);
-//   }
+/**
+ * Extract css from example.html file
+ * @param {*} data
+ */
+function resolveCSS(data) {
+  const r1 = regStyle.exec(data);
+  const newCss = r1[0].replace("<style>", "").replace("</style>", "");
+  //   console.log(r1);
+  const wf = path.join(__dirname, "/files/index.css");
+  fs.writeFile(wf, newCss, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log("Write CSS file success");
+  });
+}
 
-//   const arrOld = data.split("\n");
-//   const arrNew = [];
+/**
+ * Extract script from example.html file
+ * @param {*} data
+ */
+function resolveScript(data) {
+  const r1 = regScript.exec(data);
+  const newJs = r1[0].replace("<script>", "").replace("</script>", "");
+  //   console.log(r1);
+  const wf = path.join(__dirname, "/files/index.js");
+  fs.writeFile(wf, newJs, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log("Write js file success");
+  });
+}
 
-//   arrOld.forEach((item) => {
-//     arrNew.push(item);
-//   });
+/**
+ * Modify HTML from example.html file
+ * @param {*} data
+ */
+function resolveHTML(data) {
+  const newHTML = data
+    .replace(
+      regStyle,
+      "<link rel='stylesheet' href='index.css' type='text/css' />"
+    )
+    .replace(
+      regScript,
+      "<script src='index.js' type='text/javascript'></script>"
+    );
+  const wf = path.join(__dirname, "/files/exampleModified.html");
+  fs.writeFile(wf, newHTML, function (err) {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log("Write CSS file success");
+  });
+}
 
-//   const arrStr = arrNew.join("\r\n");
+const regStyle = /<style>[\s\S]*<\/style>/g; // css tag regexp
+const regScript = /<script>[\s\S]*<\/script>/g; // script tag regexp
 
-//   console.log(arrStr);
-// });
-
+// read html file
 const rf = path.join(__dirname, "/files/example.html");
-
-const regStyle = /<style>[\s\S]*<\/style>/g;
-const regScript = /<script>[\s\S]*<\/script>/g;
 
 fs.readFile(rf, "utf8", function (err, data) {
   if (err) {
     return console.error(err);
   }
-
   resolveCSS(data);
   resolveScript(data);
+  resolveHTML(data);
 });
-
-// extract css in example.html file
-
-function resolveCSS(data) {
-  const r1 = regStyle.exec(data);
-  const newCss = r1[0].replace("<style>", "").replace("</style>", "");
-//   console.log(r1);
-  const wf = path.join(__dirname, "/files/index.css")
-  fs.writeFile(wf, newCss, function(err){
-    if (err) {
-        return console.error(err);
-      }
-    return console.log('Write CSS file success');
-  });
-}
-
-function resolveScript(data) {
-    const r1 = regScript.exec(data);
-    const newCss = r1[0].replace("<script>", "").replace("</script>", "");
-  //   console.log(r1);
-    const wf = path.join(__dirname, "/files/index.js")
-    fs.writeFile(wf, newCss, function(err){
-      if (err) {
-          return console.error(err);
-        }
-      return console.log('Write js file success');
-    });
-  }
-  
